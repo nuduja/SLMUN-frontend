4921363fea8f871f70a6ae63bac903fe26c0eea6
nuduja <nudujabandara@gmail.com>
2022-09-01 09:27:51 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import DashboardLayout from "./layouts/dashboard";
+import DelegateLayout from "./layouts/Delegate";
+// import ReactDOM from "react-dom";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<DashboardLayout />} />
+        <Route path="/delegate" element={<DelegateLayout />} />
+        {/* <Route path="*" element={<NoPage />} /> */}
+      {/* </Route> */}
+    </Routes>
+  </BrowserRouter>
+  // <a href="http://localhost:3000/dashboard">Hellow</a>
   );
 }
-
+// ReactDOM.render(<App />, document.getElementById("root"));
 export default App;
